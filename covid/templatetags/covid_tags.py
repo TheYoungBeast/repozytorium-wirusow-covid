468276506f6e52b893b34798e7b23fbf8d8d387c
Dominik Dominik <nostrada.muss@wp.pl>
2021-06-15 21:27:34 +0200

    Add files via upload

---
+++ covid/templatetags/covid_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from covid.models import Genome
+
+register = template.Library()
+
+def genomes_total():
+    return Genome.objects.all().count()
+
+def relative_url(field_name, value, urlencode=None):
+    url = '?{}={}'.format(field_name, value)
+
+    if urlencode:
+        querystring = urlencode.split('&')
+        filtered_querystring = filter(lambda p: p.split('=')[0] != field_name, querystring)
+        encoded_querystring = '&'.join(filtered_querystring)
+        url = '{}&{}'.format(url, encoded_querystring)
+
+    return url
+
+
+register.simple_tag(relative_url)
+register.simple_tag(genomes_total)
